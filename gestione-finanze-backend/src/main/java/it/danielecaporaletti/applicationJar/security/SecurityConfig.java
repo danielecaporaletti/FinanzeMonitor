9e7584c8b9742db583dbcc9bfe42c5ba04188eb3
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-05-27 05:02:26 +0200

    add import

--- gestione-finanze-backend/src/main/java/it/danielecaporaletti/applicationJar/security/SecurityConfig.java
+++ gestione-finanze-backend/src/main/java/it/danielecaporaletti/applicationJar/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 
